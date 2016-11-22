42d462598ee7b561e9b44a297cdb42c69669f4d6
JangoBrick <mail@jangobrick.de>
2016-11-22 20:00:06 +0100

    Add ButtonSwap for JButton

---
+++ src/main/java/de/jangobrick/swapper/components/ButtonSwap.java
@@ -0,0 +1,45 @@
+package de.jangobrick.swapper.components;
+
+import javax.swing.Icon;
+import javax.swing.JButton;
+
+import de.jangobrick.swapper.Swap;
+
+
+/**
+ * Swap class for {@link JButton}. Allows setting text and icon (both optional).
+ */
+public class ButtonSwap extends Swap<JButton>
+{
+    @Override
+    protected JButton build()
+    {
+        return new JButton();
+    }
+
+    /**
+     * Sets the text displayed on this button.
+     * 
+     * @param text The text to display.
+     * 
+     * @return This instance.
+     */
+    public ButtonSwap text(String text)
+    {
+        getComponent().setText(text);
+        return this;
+    }
+
+    /**
+     * Sets the icon displayed on this button.
+     * 
+     * @param icon The icon to display.
+     * 
+     * @return This instance.
+     */
+    public ButtonSwap icon(Icon icon)
+    {
+        getComponent().setIcon(icon);
+        return this;
+    }
+}
