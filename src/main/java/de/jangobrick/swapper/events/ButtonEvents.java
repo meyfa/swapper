9e430599180d7b369ea8a1f86c7bed2996afa194
JangoBrick <mail@jangobrick.de>
2016-11-22 20:02:35 +0100

    Add ButtonEvents for ActionListener on ButtonSwap

---
+++ src/main/java/de/jangobrick/swapper/events/ButtonEvents.java
@@ -0,0 +1,19 @@
+package de.jangobrick.swapper.events;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.AbstractButton;
+
+
+/**
+ * Collection of {@link Event} constants for buttons.
+ */
+public class ButtonEvents
+{
+    /**
+     * Registers an {@link ActionListener} on the {@link AbstractButton}.
+     */
+    public static final Event<AbstractButton, ActionListener> ACTION = (c, l) -> {
+        c.addActionListener(l);
+    };
+}
