bfe28d372fa18c27bdc096268db7908b141ab7de
JangoBrick <mail@jangobrick.de>
2016-11-22 19:58:36 +0100

    Add LabelSwap for JLabel

---
+++ src/main/java/de/jangobrick/swapper/components/LabelSwap.java
@@ -0,0 +1,45 @@
+package de.jangobrick.swapper.components;
+
+import javax.swing.Icon;
+import javax.swing.JLabel;
+
+import de.jangobrick.swapper.Swap;
+
+
+/**
+ * Swap class for {@link JLabel}. Allows setting text and icon (both optional).
+ */
+public class LabelSwap extends Swap<JLabel>
+{
+    @Override
+    protected JLabel build()
+    {
+        return new JLabel();
+    }
+
+    /**
+     * Sets the text displayed on this label.
+     * 
+     * @param text The text to display.
+     * 
+     * @return This instance.
+     */
+    public LabelSwap text(String text)
+    {
+        getComponent().setText(text);
+        return this;
+    }
+
+    /**
+     * Sets the icon displayed on this label.
+     * 
+     * @param icon The icon to display.
+     * 
+     * @return This instance.
+     */
+    public LabelSwap icon(Icon icon)
+    {
+        getComponent().setIcon(icon);
+        return this;
+    }
+}
