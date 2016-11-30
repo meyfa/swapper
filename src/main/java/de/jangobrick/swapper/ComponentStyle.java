e5f56371265fa287b21217961b4d294fc804ae8f
JangoBrick <mail@jangobrick.de>
2016-11-30 22:09:12 +0100

    Add `border` and `opaque` setters to ComponentStyle

--- src/main/java/de/jangobrick/swapper/ComponentStyle.java
+++ src/main/java/de/jangobrick/swapper/ComponentStyle.java
@@ -5,10 +5,19 @@ import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Font;
 
+import javax.swing.JComponent;
+import javax.swing.border.Border;
+
 
 /**
  * This class controls a Swap's appearance or style by providing setters for the
  * most common properties like font, background or cursor.
+ * 
+ * <p>
+ * Some properties might not be supported by every instance of this class; for
+ * example, border, or whether the component is opaque, are limited to
+ * subclasses of {@code JComponent}. In case of an unsupported property being
+ * set anyway, this class guarantees not to throw.
  */
 public class ComponentStyle
 {
@@ -71,6 +80,46 @@ public class ComponentStyle
     public ComponentStyle cursor(Cursor cursor)
     {
         component.setCursor(cursor);
+        return this;
+    }
+
+    /**
+     * Sets whether the component shall be opaque, i.e. whether it shall paint
+     * every pixel inside its bounds (for a value of {@code true}) or allow the
+     * background to show through (for a value of {@code false}, the default).
+     * 
+     * <p>
+     * This requires the component to be a subclass of {@code JComponent}.
+     * 
+     * @param o Whether the component shall be opaque.
+     * 
+     * @return This instance.
+     */
+    public ComponentStyle opaque(boolean o)
+    {
+        if (!(component instanceof JComponent)) {
+            return this;
+        }
+        ((JComponent) component).setOpaque(o);
+        return this;
+    }
+
+    /**
+     * Sets the component's border.
+     * 
+     * <p>
+     * This requires the component to be a subclass of {@code JComponent}.
+     * 
+     * @param border The new border.
+     * 
+     * @return This instance.
+     */
+    public ComponentStyle border(Border border)
+    {
+        if (!(component instanceof JComponent)) {
+            return this;
+        }
+        ((JComponent) component).setBorder(border);
         return this;
     }
 }
